9b83bca03403194477b39326618a56346b582243
GakTau24 <adhiy249@gmail.com>
2023-07-25 20:12:02 +0700

    update

--- components/HomePage/Trending.tsx
+++ components/HomePage/Trending.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/free-mode";
 import "swiper/css";
 
 type TrendingItem = {
