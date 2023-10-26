b86d3ebbe237bf513b71f669f8c0f751971bbfe5
GakTau24 <adhiy249@gmail.com>
2023-10-26 21:03:16 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,46 +5,69 @@ import PcGames from "@/components/HomePage/PcGames";
 import Apps from "@/components/HomePage/Apps";
 import Carousel from "@/components/HomePage/Carousel";
 import Voucher from "@/components/HomePage/Voucher";
+import axios from "axios";
 
-export const metadata: Metadata = {
-  title: `Topup Game | ${process.env.NEXT_PUBLIC_SITE_NAME}`,
-  description:
-    "Top-up game online dengan harga murah dan proses cepat. Dapatkan bonus menarik saat membeli game online di Reyvin Store.",
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-  },
-  manifest: "/manifest.json",
-  icons:
-    "https://media.discordapp.net/attachments/987438938966872186/1135192344984043540/REYVN_LOGO.png?width=284&height=402",
-  keywords: [
-    "reyvin store",
-    "reyvinstore",
-    "topup reyvinsotre",
-    "Top Up dan Jual Voucher Game Termurah dan Lengkap",
-    `Tempat Top up Game murah cepat dan terpercaya - ${process.env.NEXT_PUBLIC_SITE_NAME}`,
-    "topup games",
-    "topup game",
-    "topup game online murah",
-    "beli diamond murah",
-    "topup mobile legends",
-    "topup pubg mobile",
-    "topup free fire",
-    "topup valorant",
-    "topup game termurah",
-    "game voucher",
-    "game online",
-  ],
-  openGraph: {
-    images: [
-      {
-        url: "https://media.discordapp.net/attachments/987438938966872186/1135192344984043540/REYVN_LOGO.png?width=284&height=402",
-        alt: "Reyvin Store",
-      },
-    ],
-  },
+const getData = async () => {
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/reyvinstore`
+  );
+  return res.data;
 };
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: any
+): Promise<Metadata> {
+  const data = await getData();
+  const title = data.map((game: any) => game.title);
+  const keywords = title.map((title: string) => `topup games ${title}`);
+
+  const previousImages = (await parent)?.openGraph?.images || [];
+  return {
+    title: `Topup Game | ${process.env.NEXT_PUBLIC_SITE_NAME}`,
+    openGraph: {
+      images: [
+        {
+          url: data.image,
+          alt: data.title,
+        },
+        ...previousImages,
+      ],
+      title: `${data.title} - ${process.env.NEXT_PUBLIC_SITE_NAME}`,
+      description: `Beli top-up game online dengan harga paling murah hanya di ${process.env.NEXT_PUBLIC_SITE_NAME}! Dapatkan harga spesial untuk top-up game seperti Mobile Legends, PUBG Mobile, Free Fire, Valorant, dan game online lainnya. tersedia dengan harga ${data.price}. Pesan sekarang dan nikmati pengalaman bermain game online yang lebih menyenangkan.`,
+      url: process.env.NEXT_PUBLIC_BASE_URL,
+    },
+    description: `Beli top-up game online dengan harga paling murah hanya di ${process.env.NEXT_PUBLIC_SITE_NAME}! Dapatkan harga spesial untuk top-up game seperti Mobile Legends, PUBG Mobile, Free Fire, Valorant, dan game online lainnya. tersedia dengan harga ${data.price}. Pesan sekarang dan nikmati pengalaman bermain game online yang lebih menyenangkan.`,
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+    },
+    manifest: "/manifest.json",
+    icons:
+      "https://media.discordapp.net/attachments/987438938966872186/1135192344984043540/REYVN_LOGO.png?width=284&height=402",
+    keywords: [
+      "reyvin store",
+      "reyvinstore",
+      "topup reyvinsotre",
+      "Top Up dan Jual Voucher Game Termurah dan Lengkap",
+      `Tempat Top up Game murah cepat dan terpercaya - ${process.env.NEXT_PUBLIC_SITE_NAME}`,
+      ...title.map((title: string) => `games ${title}`),
+      ...keywords,
+      "topup game online murah",
+      "beli diamond murah",
+      "topup game termurah",
+      ...title.map(
+        (title: string) =>
+          `${process.env.NEXT_PUBLIC_SITE_NAME} topup games ${title}`
+      ),
+    ],
+  };
+}
 
 export default function Home() {
   return (
