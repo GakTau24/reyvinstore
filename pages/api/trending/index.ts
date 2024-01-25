54b2e2834d29fd95e244a225e734a99777bd94fb
unknown <adhiy249.com>
2024-01-25 20:09:39 +0700

    update UI

--- pages/api/trending/index.ts
+++ pages/api/trending/index.ts
@@ -3,13 +3,9 @@ import Trending from "@/models/trending";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next"
 import Nextauth from "../auth/[...nextauth]";
-import * as https from 'https';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   const session = await getServerSession(request, response, Nextauth)
-  const ipAddress = request.connection.remoteAddress
-
-  
   if (request.method === "POST") {
     if (!session) {
       return response.status(401).json({ error: "Anda belum Login" })
@@ -21,8 +17,6 @@ export default async function handler(request: NextApiRequest, response: NextApi
       await Trending.create({ slug, image, title, price });
 
       response.status(201).json({ message: "Data telah berhasil dibuat!" });
-      
-      await sendToDiscord(ipAddress);
     } catch (error) {
       response.status(500).json({ message: "Kesalahan Server Internal" });
     }
@@ -32,8 +26,6 @@ export default async function handler(request: NextApiRequest, response: NextApi
 
       const trending = await Trending.find();
       response.status(200).json({ trending });
-      
-      await sendToDiscord(ipAddress);
     } catch (error) {
       response.status(500).json({ message: "Kesalahan Server Internal" });
     }
@@ -54,32 +46,5 @@ export default async function handler(request: NextApiRequest, response: NextApi
     } catch (error) {
       response.status(500).json({ message: "Kesalahan Server Internal" });
     }
-  }
-
-  async function sendToDiscord(ipAddress: any) {
-    const webhook = process.env.WEBHOOK
-    const res = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.API_KEY}&ip=${ipAddress}`);
-    const data = await res.json();
-    const embed = {
-      title: "REQUEST",
-      description: `
-        **IP Address:** ${data.ip}
-        **Country:** ${data.country_name}
-        **City:** ${data.city}
-      `,
-      color: 16711680
-    }
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    }
-    const req = https.request(webhook!, options, (res) => {
-      console.log('Discord Webhook response status:', res.statusCode);
-    });
-
-    req.write(JSON.stringify({ embeds: [embed] }));
-    req.end();
   }
 }
